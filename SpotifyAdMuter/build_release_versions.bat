53d2794ebf0c8f5a242e5065f1dfbedd288419a7
Zintom <tzsinkoapps@gmail.com>
2020-12-24 13:56:30 +0000

    Release build is no longer self contained.

--- SpotifyAdMuter/build_release_versions.bat
+++ SpotifyAdMuter/build_release_versions.bat
@@ -2,13 +2,13 @@
 
 echo Building Windows 64-Bit release..
 echo.
-dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -r win-x64
+dotnet publish -c Release --self-contained false -r win-x64
 echo Done.
 echo.
 
 echo Building Windows 32-Bit release..
 echo.
-dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -r win-x86
+dotnet publish -c Release --self-contained false -r win-x86
 echo Done.
 
 echo.
