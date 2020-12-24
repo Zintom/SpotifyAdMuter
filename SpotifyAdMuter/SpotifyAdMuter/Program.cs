f0f5005fe1f940c46c39b535670c1228f7ef716e
Zintom <tzsinkoapps@gmail.com>
2020-12-24 12:56:14 +0000

    Create Program.cs
    This program runs in the System Tray by default, there is no main form.

---
+++ SpotifyAdMuter/SpotifyAdMuter/Program.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Windows.Forms;
+
+namespace SpotifyAdMuter
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new TrayApplicationContext());
+        }
+    }
+
+    public class TrayApplicationContext : ApplicationContext
+    {
+        private readonly MuterService muterService = new MuterService();
+
+        private readonly NotifyIcon _trayIcon;
+
+        public TrayApplicationContext()
+        {
+            ContextMenuStrip menuStrip = new ContextMenuStrip();
+            menuStrip.Items.Add(new ToolStripMenuItem("Exit", null, Exit));
+
+            // Initialize Tray Icon
+            _trayIcon = new NotifyIcon()
+            {
+                Icon = Properties.Resources.AppIcon,
+                ContextMenuStrip = menuStrip,
+                Visible = true
+            };
+
+            _trayIcon.BalloonTipTitle = "Spotify Muter Service";
+            _trayIcon.BalloonTipText = "I'm running in the system tray.";
+            _trayIcon.ShowBalloonTip(3000);
+
+            muterService.BeginMuting();
+        }
+
+        void Exit(object? sender, EventArgs e)
+        {
+            muterService.EndMuting();
+
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            _trayIcon.Visible = false;
+            _trayIcon.Dispose();
+
+            Application.Exit();
+        }
+    }
+}
