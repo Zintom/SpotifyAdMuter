2aff2b6e69e7e5b0df17f49837dd636fdbe7a9bb
Zintom <tzsinkoapps@gmail.com>
2020-12-24 13:00:11 +0000

    Update build_release_versions.bat

--- SpotifyAdMuter/build_release_versions.bat
+++ SpotifyAdMuter/build_release_versions.bat
@@ -10,18 +10,6 @@ echo Building Windows 32-Bit release..
 echo.
 dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -r win-x86
 echo Done.
-echo.
-
-echo Building LINUX 64-BIT release..
-echo.
-dotnet publish -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -r linux-x64
-echo Done.
-echo.
-
-echo Building Cross-platform binaries..
-echo.
-dotnet publish -c Release
-echo Done.
 
 echo.
 echo.
