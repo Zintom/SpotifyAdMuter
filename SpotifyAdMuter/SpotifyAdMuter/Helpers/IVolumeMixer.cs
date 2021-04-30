1e61bc1a8f15fd0e32440fe0f49e5e311f5fa2ea
Zintom <tzsinkoapps@gmail.com>
2021-04-30 20:12:29 +0100

    Now implements NAudio interfaces
    The VolumeMixer implementation was very "bodge-y" and basically was a cut down version of what NAudio.Wasapi does. For this reason I have decided to opt for the well tested NAudio implementation.
    
    This means I now have access to not just the DefaultAudioEndpoint, but all of them(EnumerateAudioEndPoints)!
    
    This means that the blocker now operates across all audio endpoints rather than just the current one (speakers usually). So if your Spotify is set to output to a Virtual Audio Cable or something similar, then the adblocker will still see it :hatched_chick:.

---
+++ SpotifyAdMuter/SpotifyAdMuter/Helpers/IVolumeMixer.cs
@@ -0,0 +1,24 @@
+﻿namespace SpotifyAdMuter.Helpers
+{
+    public interface IVolumeMixer
+    {
+        /// <summary>
+        /// Gets the volume of the audio session associated with the given <paramref name="processID"/>.
+        /// </summary>
+        /// <param name="processID"></param>
+        /// <returns>A floating value between 0 and 1 which represents the volume of the session.</returns>
+        public float? GetApplicationVolume(int processID);
+
+        /// <summary>
+        /// Sets the volume of the audio session associated with the given <paramref name="processID"/>.
+        /// </summary>
+        /// <param name="level">The volume level to set it to, from 0 to 1</param>
+        public void SetApplicationVolume(int processID, float level);
+
+        /// <summary>
+        /// Mutes the audio session associated with the given <paramref name="processID"/>.
+        /// </summary>
+        /// <param name="mute">Mute = <see langword="true"/>, Unmute = <see langword="false"/>.</param>
+        public void SetApplicationMute(int processID, bool mute);
+    }
+}
---
+++ SpotifyAdMuter/SpotifyAdMuter/Helpers/NAudioVolumeMixer.cs
@@ -0,0 +1,54 @@
+﻿using NAudio.CoreAudioApi;
+
+namespace SpotifyAdMuter.Helpers
+{
+    public class NAudioVolumeMixer : IVolumeMixer
+    {
+        public float? GetApplicationVolume(int processID)
+        {
+            AudioSessionControl? asc = GetAudioSession(processID);
+
+            return asc?.SimpleAudioVolume?.Volume ?? null;
+        }
+
+        public void SetApplicationMute(int processID, bool mute)
+        {
+            AudioSessionControl? asc = GetAudioSession(processID);
+            if (asc == null) return;
+
+            asc.SimpleAudioVolume.Mute = mute;
+        }
+
+        public void SetApplicationVolume(int processID, float level)
+        {
+            AudioSessionControl? asc = GetAudioSession(processID);
+            if (asc == null) return;
+
+            asc.SimpleAudioVolume.Volume = level;
+        }
+
+        /// <summary>
+        /// Gets the audio session for the given process, if that process exists and has an audio session.
+        /// </summary>
+        private static AudioSessionControl? GetAudioSession(int processID)
+        {
+            using (var deviceEnumerator = new MMDeviceEnumerator())
+            {
+                foreach (var device in deviceEnumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+                {
+                    var sessions = device.AudioSessionManager.Sessions;
+
+                    for (int i = 0; i < sessions.Count; i++)
+                    {
+                        if (sessions[i].GetProcessID == processID)
+                        {
+                            return sessions[i];
+                        }
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
