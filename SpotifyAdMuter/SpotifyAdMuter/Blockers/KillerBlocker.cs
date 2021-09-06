3658f83c3c0a7745aebc571853a8dcf29b2830b5
Zintom <tzsinkoapps@gmail.com>
2021-09-06 15:36:48 +0100

    Create KillerBlocker.cs
    This is an advert blocker that uses an exploit in the way Spotify handles adverts when the application is restarted.
    
    When the Spotify application exists, any playing ads are removed from the queue, and the next song that was meant to play is selected, when you relaunch the application, it primes the next song, all it takes is a press of the play button and away you go.
    
    This IAdvertBlocker kills the Spotify processes when an advert is detected, eliminating the adverts, relaunches Spotify, it then sends a low level key event which simulates the Space bar being pressed, Spotify treats this as a press of the Play button, resuming the queue.

---
+++ SpotifyAdMuter/SpotifyAdMuter/Blockers/KillerBlocker.cs
@@ -0,0 +1,54 @@
+﻿using SpotifyAdMuter.SendInput;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SpotifyAdMuter.Blockers
+{
+    /// <summary>
+    /// Blocks Spotify ads by killing the app, then relaunching.
+    /// </summary>
+    public class KillerBlocker : IAdvertBlocker
+    {
+
+        private long _lastKillEvent = 0;
+
+        public void Block()
+        {
+            // We need to give time so that we aren't killing the application too often.
+            if (DateTimeOffset.Now.ToUnixTimeMilliseconds() - _lastKillEvent < 15000)
+            {
+                return;
+            }
+
+            Process[] spotifyProcesses = Process.GetProcessesByName("Spotify");
+
+            if (spotifyProcesses.Length == 0) { return; }
+
+            for (int i = 0; i < spotifyProcesses.Length; i++)
+            {
+                if (!spotifyProcesses[i].HasExited)
+                    spotifyProcesses[i].Kill();
+            }
+
+            // Restart the process.
+            string? spotifyExePath = spotifyProcesses[0].MainModule?.FileName;
+            if (spotifyExePath == null) { return; }
+
+            Process.Start(spotifyExePath);
+
+            // Give the application time to launch!
+            Thread.Sleep(2000);
+
+            InputSim.SendInputKeys(Keys.Space);
+
+            _lastKillEvent = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        }
+
+        public void Unblock()
+        {
+
+        }
+    }
+}
